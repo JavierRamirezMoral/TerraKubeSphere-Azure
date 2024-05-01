9fbb9d28e9bfe95f97d0440efde456fa22e60e08
Javier Ramírez Moral <101793125+JavierRamirezMoral@users.noreply.github.com>
2024-05-01 09:57:46 +0200

    Delete Files/Kubernetes/outputs.tf

--- Files/Kubernetes/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value     = azurerm_resource_group.RG
-  sensitive = true
-}
-
-output "kubernetes_cluster_name" {
-  value     = azurerm_kubernetes_cluster.AKS
-  sensitive = true
-}
